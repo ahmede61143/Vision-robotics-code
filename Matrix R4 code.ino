41b03af696e0e1d6bd0642134d58e78ac578d98e
ahmede61143 <ahmede61143@gmail.com>
2026-04-28 11:35:59 +0300

    Refactor WiFi setup and enhance code comments
    Updated WiFi credentials and added comments for clarity.

--- Matrix R4 code.ino
+++ Matrix R4 code.ino
@@ -2,8 +2,8 @@
 #include <WiFi.h>
 #include <Wire.h>
 
-const char* ssid = "Not showen for privecy reasons";
-const char* password = "Not showen for privecy reasons";
+const char* ssid = "Your_WiFi_SSID";
+const char* password = "Your_WiFi_Password";
 WiFiUDP udp;
 
 enum State { IDLE, MOVE_10CM, LOCAL_SEARCH, FINISHED };
@@ -14,34 +14,50 @@ void setup() {
   Mini.imu.begin();
   Wire.begin(); 
   
+  Serial.begin(115200);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) delay(500);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  // Print IP so you can put it in the Python script
+  Serial.println("\nRobot Connected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP()); 
+
   udp.begin(4210);
-  delay(2000); // Calibration time
+  delay(2000); // Calibration time for IMU [cite: 7]
 }
 
 void loop() {
   switch (robotState) {
     case IDLE:
-      if (udp.parsePacket() && udp.read() == 'F') robotState = MOVE_10CM;
+      // Wait for 'F' trigger from PC [cite: 7]
+      if (udp.parsePacket() && udp.read() == 'F') {
+        robotState = MOVE_10CM;
+      }
       break;
 
     case MOVE_10CM:
-      driveStraight(50, 1200); // Uses built-in Gyro
+      driveStraight(50, 1200); // 1.2 seconds of movement [cite: 8]
       robotState = LOCAL_SEARCH;
       break;
 
     case LOCAL_SEARCH:
-      Wire.requestFrom(0x12, 2);
+      Wire.requestFrom(0x12, 2); // Request data from Camera [cite: 10]
       if (Wire.available() >= 2) {
         int cx = (Wire.read() << 8) | Wire.read();
-        if (cx == 0) return; 
         
-        if (cx < 140) { // Steer Left
-          Mini.M1.set(30); Mini.M2.set(30); Mini.M3.set(50); Mini.M4.set(50);
-        } else if (cx > 180) { // Steer Right
-          Mini.M1.set(50); Mini.M2.set(50); Mini.M3.set(30); Mini.M4.set(30);
-        } else { // Centered
+        if (cx == 0) return; // Ignore if no target
+        
+        if (cx < 140) { // Steer Left [cite: 11]
+          Mini.M1.set(20); Mini.M2.set(20);
+          Mini.M3.set(40); Mini.M4.set(40);
+        } else if (cx > 180) { // Steer Right [cite: 12]
+          Mini.M1.set(40); Mini.M2.set(40);
+          Mini.M3.set(20); Mini.M4.set(20);
+        } else { // Centered [cite: 13]
           robotState = FINISHED;
         }
       }
@@ -55,10 +71,10 @@ void loop() {
 
 void driveStraight(int speed, int duration) {
   long start = millis();
-  float target = Mini.imu.getAngleZ();
+  float target = Mini.imu.getAngleZ(); // Lock current heading [cite: 16]
   while (millis() - start < duration) {
     float error = target - Mini.imu.getAngleZ();
-    int corr = (int)(error * 2.5);
+    int corr = (int)(error * 2.5); // Gyro correction [cite: 18]
     Mini.M1.set(speed + corr); Mini.M2.set(speed + corr);
     Mini.M3.set(speed - corr); Mini.M4.set(speed - corr);
   }
